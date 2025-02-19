ce8e7e78df05c032423e61507117b1e1f977490c
thiago alves <thiago87gsa@gmail.com>
2025-02-19 11:53:01 -0300

    :sparkles: feat: hero part 3 - most popular responsiveness

--- src/components/home/hero/FloatingImage.tsx
+++ src/components/home/hero/FloatingImage.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 
 const FloatingImage = () => {
   return (
-    <div className="bg-white shadow-lg w-max h-[230px] overflow-hidden flex items-center justify-center">
+    <div className=" h-[250px] sm:w-max w-fit overflow-hidden flex items-center justify-center">
       <Image
         alt="bicicleta amarela"
         src={"/img/yellowBike.jpg"}
-        width={400}
-        height={400}
+        width={600}
+        height={600}
+        className="w-[450px]"
       />
     </div>
   );
--- src/components/home/hero/HeroPart2.tsx
+++ src/components/home/hero/HeroPart2.tsx
@@ -4,55 +4,59 @@ import FloatingImage from "./FloatingImage";
 const HeroPart2 = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center py-20">
-      <div className="flex  w-full max-w-[1160px]">
-        <div className="flex flex-col w-1/2 pr-14 mt-24">
-          <div className="relative">
-            <div className="lg:absolute lg:-top-[308px] lg:-left-[8px] lg:right-96">
+      <div className="flex flex-col lg:flex-row items-center lg:items-stretch w-full max-w-[1160px]">
+        <div className="flex items-center lg:items-stretch flex-col w-1/2 lg:pr-14 lg:mt-24">
+          <div className="flex flex-col items-center lg:items-stretch relative">
+            <div className="lg:absolute lg:-top-[308px] lg:-left-[8px] lg:right-96 w-[318px] sm:w-[450px]">
               <FloatingImage />
             </div>
-            <p className="text-red-500 font-bold text-lg mb-1">Most Popular</p>
+            <p className="text-red-500 font-bold text-lg lg:mb-1 lg:my-0 my-10">
+              Most Popular
+            </p>
           </div>
-          <h2 className=" font-bold text-4xl mb-10 w-[80%]">
+          <h2 className=" font-bold text-4xl mb-10 w-[180%] lg:w-[80%] text-center lg:text-left">
             Want To Take Cycling To The Next Level?
           </h2>
-          <p className="mb-10">
+          <p className="mb-10 w-screen lg:w-full text-center lg:text-left px-5 sm:px-28 lg:px-0">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
             tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
           </p>
-          <div className="flex gap-20 mb-10">
-            <div className="flex flex-col gap-4">
+          <div className="flex flex-col sm:flex-row items-center sm:items-stretch lg:gap-20 mb-16 lg:mb-10 w-screen lg:w-full justify-around lg:justify-start gap-8">
+            <div className="flex flex-col gap-2 sm:gap-4 text-center">
               <p>Fork Travel</p>
-              <h3 className="font-bold text-xl italic">205 MM</h3>
+              <h3 className="font-bold text-2xl sm:text-xl italic">205 MM</h3>
             </div>
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2 sm:gap-4 text-center">
               <p>Material</p>
-              <h3 className="font-bold text-xl italic">Carbon</h3>
+              <h3 className="font-bold text-2xl sm:text-xl italic">Carbon</h3>
             </div>
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2 sm:gap-4 text-center">
               <p>Weight</p>
-              <h3 className="font-bold text-xl italic">12.6 KG</h3>
+              <h3 className="font-bold text-2xl sm:text-xl italic">12.6 KG</h3>
             </div>
           </div>
-          <button className="bg-red-500 text-white font-semibold uppercase w-max px-8 py-5 hover:bg-red-700 transition-colors duration-200">
+          <button className="bg-red-500 text-white font-semibold uppercase w-max px-8 mb-16 py-5 hover:bg-red-700 transition-colors duration-200">
             Show More
           </button>
         </div>
-        <div className="w-1/2">
-          <div className="relative mt-40">
-            <Image
-              alt="men riding bicycles"
-              src={"/img/bikes3.jpg"}
-              width={600}
-              height={600}
-              className="w-[540px]"
-            />
-            <div className="flex flex-col items-center justify-center gap-5 absolute p-10 -top-40 -right-0 w-[360px] h-[200px] bg-red-600">
+        <div className="flex flex-col items-center lg:items-stretch w-full lg:w-1/2 ">
+          <div className="flex flex-col items-center lg:items-stretch lg:relative lg:mt-40 px-4 lg:px-0">
+            <div className="lg:absolute p-4 lg:p-10 lg:-top-40 lg:-right-0 flex flex-col items-center justify-center gap-5  lg:w-[360px] h-[200px] bg-red-500 text-center lg:text-left mb-16 lg:mb-0">
               <h2 className="text-white text-3xl font-bold">
                 The most popular bikes of the season!
               </h2>
               <button className="uppercase text-white font-bold border-dotted border-white border-2 py-4 px-8">
                 shop now
               </button>
+            </div>
+            <div>
+              <Image
+                alt="men riding bicycles"
+                src={"/img/bikes3.jpg"}
+                width={600}
+                height={600}
+                className="w-[800px] lg:w-[540px]"
+              />
             </div>
           </div>
         </div>
