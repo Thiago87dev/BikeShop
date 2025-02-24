9d0d8fe6f3564b63e3115241aa66d2dbcde9dc83
thiago alves <thiago87gsa@gmail.com>
2025-02-24 09:45:34 -0300

    :sparkles: feat: best offer part 2 - responsiveness

--- src/components/home/bestOffer/BestOffer.tsx
+++ src/components/home/bestOffer/BestOffer.tsx
@@ -7,9 +7,9 @@ import { AiOutlinePercentage } from "react-icons/ai";
 const BestOffer = () => {
   return (
     <div className=" flex justify-center">
-      <div className="flex  justify-between w-full max-w-[1160px] my-20">
-        <div className="flex">
-          <div>
+      <div className="flex flex-col lg:flex-row items-center lg:items-stretch justify-between w-full max-w-[1160px] my-20">
+        <div className="flex flex-col lg:flex-row items-center lg:items-stretch">
+          <div className="hidden lg:block">
             <div className="mb-8">
               <Image
                 alt="handlebar"
@@ -26,12 +26,27 @@ const BestOffer = () => {
             <Image
               alt="man in a bike"
               src={"/img/bestOffer/pexels-cottonbro-600x900.jpg"}
-              width={280}
-              height={280}
+              width={600}
+              height={600}
+              className="w-screen px-6 lg:w-[280px]"
             />
           </div>
+          <div className="flex flex-col items-center lg:hidden gap-5 mt-5 ">
+            <div className="flex items-center justify-center text-3xl font-bold bg-red-500 text-white text-center w-[95%] h-[100px]">
+              Save $500 on the Orbea Rise M-Ltd Bike!
+            </div>
+            <div className="mb-8">
+              <Image
+                alt="handlebar"
+                src={"/img/bestOffer/guidao.jpg"}
+                width={600}
+                height={600}
+                className="w-screen px-6"
+              />
+            </div>
+          </div>
         </div>
-        <div className=" w-1/3 mr-20">
+        <div className="flex flex-col items-center lg:items-stretch text-center lg:text-left w-full px-6 sm:px-0 sm:w-[70%] lg:w-1/3 mr-0 lg:mr-20">
           <h3 className="text-red-500 font-bold text-xl">best offer</h3>
           <h3 className="text-4xl font-bold capitalize my-8">
             Ride more for less with Polygon bikes
@@ -40,8 +55,8 @@ const BestOffer = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna.
           </p>
-          <div className="flex gap-10 justify-between h-[140px]">
-            <div className="flex flex-col justify-between w-full">
+          <div className="flex flex-col gap-8 sm:flex-row sm:gap-10 justify-between h-[140px] text-left w-full">
+            <div className="flex flex-col gap-8 sm:gap-0 justify-between w-full">
               <div className="flex items-center gap-4">
                 <div className="bg-red-500 flex items-center justify-center p-2 text-white">
                   <PiBoundingBoxBold size={25} />
@@ -61,7 +76,7 @@ const BestOffer = () => {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col justify-between w-full">
+            <div className="flex flex-col gap-8 sm:gap-0  justify-between w-full">
               <div className="flex items-center gap-4">
                 <div className="bg-red-500 flex items-center justify-center p-2 text-white">
                   <AiOutlinePercentage size={25} />
@@ -71,11 +86,11 @@ const BestOffer = () => {
                   <p>Material</p>
                 </div>
               </div>
-              <div className="flex items-center gap-4">
+              <div className="flex items-center gap-4 ">
                 <div className="bg-red-500 flex items-center justify-center p-2 text-white">
                   <FaRuler size={25} strokeWidth={20} />
                 </div>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2 ">
                   <h3 className="font-bold text-xl italic">59.1-69</h3>
                   <p>Rider Height</p>
                 </div>
