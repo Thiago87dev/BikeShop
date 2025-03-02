3df302a5b23e8409506074a4602c29a01e4de825
thiago alves <thiago87gsa@gmail.com>
2025-03-02 09:37:10 -0300

    :sparkles: feat: most popular post - part 2 - responsiveness

--- src/components/home/mostPopularPost/MostPopularPost.tsx
+++ src/components/home/mostPopularPost/MostPopularPost.tsx
@@ -21,15 +21,15 @@ const MostPopularPost = () => {
   return (
     <div className="flex justify-center bg-[#f7f7f7] ">
       <div className="flex flex-col items-center w-full px-4">
-        <div className="flex flex-col justify-center gap-5 w-[460px] pt-20 pb-14">
+        <div className="flex flex-col justify-center gap-5 w-full sm:w-[460px] pt-20 pb-14">
           <h3 className="text-red-500 font-bold text-xl text-center">Latest News</h3>
-          <h2 className="font-bold text-4xl text-center ">Most Popular Post</h2>
+          <h2 className="font-bold text-3xl sm:text-4xl text-center ">Most Popular Post</h2>
           <p className="text-center">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
             tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
           </p>
         </div>
-        <div className="grid grid-cols-3 gap-5 w-full pb-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 w-full pb-10">
           {postData
             .sort((a, b) => b.popularity - a.popularity)
             .slice(0, 3)
