db309da85d067e17694f6dbeff590effa759009f
thiago alves <thiago87gsa@gmail.com>
2025-03-25 11:19:45 -0300

    :sparkles: feat: page contact - part 3 - accordion and contact form complete

--- src/components/contact/canWeHelpYou/CanWeHelpYou.tsx
+++ src/components/contact/canWeHelpYou/CanWeHelpYou.tsx
@@ -4,14 +4,14 @@ import Modal from "./Modal";
 const CanWeHelpYou = () => {
   return (
     <div className="flex justify-center bg-[#f7f7f7] py-20">
-      <div className="flex gap-20 w-full max-w-[1240px] px-4">
-        <div className="w-1/2 h-auto">
+      <div className="flex flex-col lg:flex-row items-center lg:items-stretch gap-20 w-full max-w-[1240px] sm:px-4">
+        <div className="lg:w-1/2 h-auto px-4 sm:px-0">
           <div className="flex flex-col gap-6">
             <h3 className="text-red-600 font-bold text-lg text-center lg:text-left uppercase ">
               Hi! Can we help you?
             </h3>
-            <h2 className="font-bold text-4xl">Popular Questions</h2>
-            <p className="text-lg w-[80%]">
+            <h2 className="font-bold text-4xl text-center lg:text-left">Popular Questions</h2>
+            <p className="text-lg lg:w-[80%] text-center lg:text-left">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus
               mauris ultrices eros in cursus turpis massa.
@@ -21,7 +21,7 @@ const CanWeHelpYou = () => {
             </div>
           </div>
         </div>
-        <div className="relative w-1/2 h-fit bg-[url('/img/contact/velobar-vMSLp_DymMs-unsplash.jpg')] bg-left bg-cover ">
+        <div className="relative w-full lg:w-1/2 h-fit bg-[url('/img/contact/velobar-vMSLp_DymMs-unsplash.jpg')] bg-left bg-cover ">
           <div className="absolute inset-0 bg-black opacity-80"></div>
           <div className="relative text-white">
             <ContactForm/>
--- src/components/contact/canWeHelpYou/ContactForm.tsx
+++ src/components/contact/canWeHelpYou/ContactForm.tsx
@@ -1,6 +1,65 @@
+"use client";
+
+import { useState } from "react";
+import { PulseLoader } from "react-spinners";
+
 const ContactForm = () => {
+  const [InfoMessage, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [inputsForm, setInfoMessage] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (
+      inputsForm.name === "" ||
+      inputsForm.email === "" ||
+      inputsForm.subject === "" ||
+      inputsForm.message === ""
+    ) {
+      setError("All fields are required");
+    } else {
+      setLoading(true);
+      setTimeout(() => {
+        setError("Submission Success, Thanks for getting in touch!");
+        setLoading(false);
+      }, 1800);
+    }
+  };
+
+  const handleFormChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+
+    setInfoMessage((prev) => {
+      const updatedForm = { ...prev, [name]: value };
+
+      if (
+        InfoMessage &&
+        updatedForm.name.trim() !== "" &&
+        updatedForm.email.trim() !== "" &&
+        updatedForm.subject.trim() !== "" &&
+        updatedForm.message.trim() !== ""
+      ) {
+        setError("");
+      }
+      return updatedForm;
+    });
+  };
+
   return (
-    <form className="flex flex-col gap-6 p-10">
+    <form onSubmit={handleSubmit} className={`flex flex-col gap-6 p-10 `}>
+      {loading && (
+        <div className="absolute flex items-center bg-black/70 backdrop-blur-sm justify-center inset-0">
+          <PulseLoader size={20} color="#bababa" />
+        </div>
+      )}
       <div className="flex flex-col">
         <label className="text-white font-bold text-xl" htmlFor="name">
           Your Name
@@ -9,6 +68,9 @@ const ContactForm = () => {
           className="outline-none bg-transparent border-b-2 border-red-600 p-3"
           type="text"
           id="name"
+          name="name"
+          value={inputsForm.name}
+          onChange={handleFormChange}
         />
       </div>
       <div className="flex flex-col">
@@ -19,6 +81,9 @@ const ContactForm = () => {
           className="outline-none bg-transparent border-b-2 border-red-600 p-3"
           type="email"
           id="email"
+          name="email"
+          value={inputsForm.email}
+          onChange={handleFormChange}
         />
       </div>
       <div className="flex flex-col">
@@ -29,6 +94,9 @@ const ContactForm = () => {
           className="outline-none bg-transparent border-b-2 border-red-600 p-3"
           type="text"
           id="subject"
+          name="subject"
+          value={inputsForm.subject}
+          onChange={handleFormChange}
         />
       </div>
       <div className="flex flex-col">
@@ -36,10 +104,26 @@ const ContactForm = () => {
           Message
         </label>
         <textarea
-          className="outline-none bg-transparent border-b-2 border-red-600 p-3 min-h-36"
-          id="subject"
+          className="outline-none bg-transparent border-b-2 border-red-600 p-3 h-28"
+          id="message"
+          name="message"
+          value={inputsForm.message}
+          onChange={handleFormChange}
         />
       </div>
+      <button className="bg-red-700 text-white font-semibold uppercase w-full px-8 mb-6 py-5 hover:bg-red-800 transition-colors duration-200 active:scale-95">
+        Send a message
+      </button>
+      {InfoMessage &&  InfoMessage !== 'Submission Success, Thanks for getting in touch!' &&(
+        <p className="text-red-500 text-lg font-bold bg-white border-[1px] border-solid border-red-500 p-2">
+          {InfoMessage}
+        </p>
+      )}
+      {InfoMessage && InfoMessage === 'Submission Success, Thanks for getting in touch!' && (
+        <p className="text-blue-500 text-center text-lg font-bold bg-white border-[1px] border-solid border-blue-500 p-2">
+          {InfoMessage}
+        </p>
+      )}
     </form>
   );
 };
